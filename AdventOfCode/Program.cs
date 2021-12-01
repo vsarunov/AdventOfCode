456f5d641a4ac70d8668ad3c5a7f13c12eb6ff15
Vlad Sarunov <Vlad.sarunov@amido.com>
2021-12-01 21:35:18 +0000

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Day 1
+            Console.WriteLine(SonarSweepDay1.GetPart1Result());
+            Console.WriteLine(SonarSweepDay1.GetPart2Result());
+        }
+    }
+}
