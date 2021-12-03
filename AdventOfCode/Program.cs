78e368fa4d76ed02b02035ba355e8d19685d751a
Vlad Sarunov <Vlad.sarunov@amido.com>
2021-12-03 21:19:27 +0000

    Day three puzzle

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -7,12 +7,16 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             // Day 1
-            Console.WriteLine(SonarSweepDay1.GetPart1Result());
-            Console.WriteLine(SonarSweepDay1.GetPart2Result());
+            //Console.WriteLine(SonarSweepDay1.GetPart1Result());
+            //Console.WriteLine(SonarSweepDay1.GetPart2Result());
 
-            // Day 2
-            Console.WriteLine(Day2.GetPart1Result());
-            Console.WriteLine(Day2.GetPart2Result());
+            //// Day 2
+            //Console.WriteLine(Day2.GetPart1Result());
+            //Console.WriteLine(Day2.GetPart2Result());
+
+            // Day 3
+            //Console.WriteLine(Day3.GetPart1Result());
+            Console.WriteLine(Day3.GetPart2Result());
         }
     }
 }
